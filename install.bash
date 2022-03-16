c6fa594e0f0410fe2db831d5da5b815bf0b5c5c2
sandravw <rzepala.marek@outlook.com>
2022-03-16 23:34:51 +0100

    !

--- install.bash
+++ install.bash
@@ -11,24 +11,24 @@ CHMOD=$(${WHICH} chmod)
 SYSTEMCTL=$(${WHICH} systemctl)
 
 timer () {
-    ${CP} -ip systemd/multimysqlbackup.service /etc/systemd/system/
-    ${CP} -ip systemd/multimysqlbackup.timer /etc/systemd/system
-    ${SYSTEMCTL} start multimysqlbackup.timer
-    ${SYSTEMCTL} enable multimysqlbackup.timer
+    ${CP} -ip systemd/improved-succotash.service /etc/systemd/system/
+    ${CP} -ip systemd/improved-succotash.timer /etc/systemd/system/
+    ${SYSTEMCTL} start improved-succotash.timer
+    ${SYSTEMCTL} enable improved-succotash.timer
 }
 
 cron () {
-    ${ECHO} '30 4 * * * root /usr/bin/sh -c "/usr/local/bin/multimysqlbackup -c /usr/local/etc/ixsqlbackup.conf.d/multimysqlbackup.conf"' >> /etc/crontab
+    ${ECHO} '30 4 * * * root /usr/bin/sh -c "/usr/local/bin/comfy-wrapper -c /usr/local/etc/improved-succotash.conf.d/comfy-wrapper.conf"' >> /etc/crontab
 }
 
-${CP} -ip ixsqlbackup.bash /usr/local/bin/ixsqlbackup
-${CP} -ip multimysqlbackup.bash /usr/local/bin/multimysqlbackup
-${CHMOD} +x /usr/local/bin/ixsqlbackup
-${CHMOD} +x /usr/local/bin/multimysqlbackup
+${CP} -ip improved-succotash.sh /usr/local/bin/improved-succotash
+${CP} -ip comfy-wrapper.sh /usr/local/bin/comfy-wrapper
+${CHMOD} +x /usr/local/bin/improved-succotash
+${CHMOD} +x /usr/local/bin/comfy-wrapper
 
-${MKDIR} /usr/local/etc/ixsqlbackup.conf.d
-${CP} -ip ixsqlbackup.conf /usr/local/etc/ixsqlbackup.conf.d/ixsqlbackup.conf
-${CP} -ip multimysqlbackup.conf /usr/local/etc/ixsqlbackup.conf.d/
+${MKDIR} /usr/local/etc/improved-succotash.conf.d
+${CP} -ip improved-succotash.conf /usr/local/etc/
+${CP} -ip comfy-wrapper.conf /usr/local/etc/ixsqlbackup.conf.d/
 
 if [[ ! $(${UNAME} -o | ${GREP} -i linux) ]] ; then
     cron
